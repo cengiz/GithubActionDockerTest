4a525ce73760481fdb0921d07aaec7be35a63b77
cengiz.kemaloglu@demirorenteknoloji.com <cengiz.kemaloglu@demirorenteknoloji.com>
2020-01-10 15:57:41 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GithubActionDockerTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
